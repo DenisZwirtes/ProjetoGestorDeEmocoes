6ee3528a775f714dde017a06e22b1bca75f661e0
denis.zwirtes <denis.zwirtes@retta.com.br>
2023-12-02 17:55:13 -0300

    Ajusta: melhoria no metodo uploadFoto.

--- app/Http/Controllers/FotoController.php
+++ app/Http/Controllers/FotoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
@@ -16,55 +17,58 @@ class FotoController extends BaseController
 
     public function uploadFoto(Request $request)
     {
-        $alterarPerfil = $request->input('alterarPerfil');
+        $this->validarRequisicao($request);
 
-        if(!$alterarPerfil){
-            $alterarPerfil = false;
+        // Obtem o nome do usuário armazenado na sessão
+        $nomeUsuario = Session::get('nome');
+        $mensagem = $this->obterMensagem();
+        $fotoPerfil = $request->file('fotoPerfil');
+        $nomeArquivo = $this->obterNomeArquivo($request, $nomeUsuario, $fotoPerfil);
+
+        // Verifica se o arquivo já existe no diretório
+        if (!File::exists(public_path('img/' . $nomeArquivo))) {
+            // Move o arquivo diretamente para o diretório desejado
+            $fotoPerfil->move(public_path('img'), $nomeArquivo);
         }
-        
+
+        // Armazena o nome do arquivo na sessão
+        Session::put('nomeArquivoFoto', $nomeArquivo);
+
+        $caminhoFoto = 'img/' . $nomeArquivo;
+
+        if (!$request->input('alterarPerfil')) {
+            return view('opcaoPerfil')->with(['caminhoFoto' => $caminhoFoto]);
+        }
+
+        return view('paginaInicial')->with(['nome' => $nomeUsuario, 'caminhoFoto' => $caminhoFoto, 'mensagem' => $mensagem]);
+    }
+
+    public function validarRequisicao($request)
+    { 
         $request->validate([
             'fotoPerfil' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
 
-        // Obtem o nome do usuário armazenado na sessão
-        $nomeUsuario = Session::get('nome');
-        $mensagem = Session::get('mensagem');
-
-        if ($nomeUsuario) {
-            $fotoPerfil = $request->file('fotoPerfil');
-            
-            if ($alterarPerfil) {
-                $nomeArquivo = $request->input('nomeArquivo');
-
-            } else {
-                $nomeArquivo = $nomeUsuario . '_' . $fotoPerfil->getClientOriginalName(). '.' . $fotoPerfil->getClientOriginalExtension();
-            }
-
-             // Verifica se o arquivo já existe no diretório
-             if (!File::exists(public_path('img/' . $nomeArquivo))) {
-                // Move o arquivo diretamente para o diretório desejado
-                $fotoPerfil->move(public_path('img'), $nomeArquivo);
-            }
-
-             // Armazena o nome do arquivo na sessão
-             Session::put('nomeArquivoFoto', $nomeArquivo);
-
-            $caminhoFoto = 'img/' . $nomeArquivo;
-
-         if (!$mensagem){
-            $mensagem = LoginController::MENSAGEM_PADRAO;
-         }
-
-         if($alterarPerfil){
-            return view('opcaoPerfil')->with(['caminhoFoto' => $caminhoFoto]);
+        if (!Session::get('nome')) {
+            throw new Exception('Usuário sem nome definido!');
         }
-
-            return view('paginaInicial')->with(['nome' => $nomeUsuario, 'caminhoFoto' => $caminhoFoto,'mensagem'=>$mensagem]);
-        }
-
-       
-
-        return redirect()->back()->with('error', 'Falha ao carregar a foto. Nome do usuário não encontrado na sessão.');
     }
 
+    public function obterNomeArquivo($request, $nomeUsuario, $fotoPerfil)
+    {
+        if (!$request->input('alterarPerfil')) {
+            return $nomeUsuario . '_' . $fotoPerfil->getClientOriginalName() . '.' . $fotoPerfil->getClientOriginalExtension();
+        }
+
+        return $request->input('nomeArquivo');
+    }
+
+    public function obterMensagem()
+    {
+        if (!Session::get('mensagem')) {
+            return LoginController::MENSAGEM_PADRAO;
+        }
+
+        return Session::get('mensagem');
+    }
 }
